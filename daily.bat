54e3ca0dc9178bfc121d394f488d6c43b9a9183a
wkokgit <wouterkok96@hotmail.com>
2020-08-23 22:28:43 +0200

    further improvements

---
+++ daily.bat
@@ -0,0 +1,2 @@
+"C:\Users\Wouter Kok\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\Wouter Kok\Documents\self-improj\src\daily.py"
+pause
--- src/daily.py
+++ src/daily.py
@@ -5,16 +5,31 @@ import json
 # Add sleep times
 labels_S = []
 entries_S = []
+text_G = []
 
 today = date.today()
 
 root=Tk()
 
-topText=Frame(root)
-topText.pack()
 
-top=Frame(root, pady=3)
+top=Frame(root)
 top.pack()
+
+intro=Frame(top, pady=3)
+intro.pack(side=LEFT)
+introTop=Frame(intro)
+introTop.pack(side=TOP)
+introText=Frame(intro)
+introText.pack()
+
+streaks_F=Frame(top, pady=3)
+streaks_F.pack(side=RIGHT)
+streaksText=Frame(streaks_F)
+streaksText.pack(side=TOP)
+streaksList = Frame(streaks_F)
+streaksList.pack()
+
+
 middle=Frame(root, pady=3)
 middle.pack()
 bottom=Frame(root, pady=3)
@@ -24,39 +39,61 @@ bottom.pack()
 def main():
     root.title("Daily Practice - " + today.strftime("%B %d, %Y"))
     
+    introduction()
     streaks()
+    
     gratitude()
 
-    B = Button(bottom, text ="Save", command = updateStreaks)
+    B = Button(bottom, text ="Save", command = update)
     B.pack()
     # Width, height in pixels
     mainloop()
 
-def streaks():
-    greeting = Label(topText, text="Streaks", font=("Arial", 16))
+def introduction():
+    greeting = Label(introTop, width=40, text="EXTRA INFO", font=("Arial", 16))
     greeting.pack()
+    
+    strin =  """-- SLEEP SCHEDULE : 6:00 OUT - 21:30 IN -- 
+                ...
+                ...
+                ..."""  
+    moreInfo = Label(introText, text=strin, font = ("Arial", 12))
+    moreInfo.pack()
+
+
+def streaks():
+    greeting = Label(streaksText, width=40, text="STREAKS", font=("Arial", 16))
+    greeting.pack(side=RIGHT)
     readStreaks()
 
 def readStreaks():
-    with open("../streaks/streaks.json", "r+") as jsonFile:
+    with open("C:/Users/Wouter Kok/Documents/self-improj/streaks/streaks.json", "r+") as jsonFile:
         data = json.load(jsonFile)
         
         i = 0
         for k,v in data.items():
-            labels_S.append(Label(top, text=k, font=("Arial",12)))
+            labels_S.append(Label(streaksList, text=k, font=("Arial",12)))
             labels_S[i].grid(row = i)
 
-            entries_S.append(Entry(top))
+            entries_S.append(Entry(streaksList))
             entries_S[i].grid(row=i, column=1)
             entries_S[i].insert(0, v)
             
             i = i + 1
 
 def gratitude():
-    greeting2 = Label(middle, text="Gratitude", font=("Arial", 16)).grid(row = 0)
-    text_box1 = Text(middle, height=5).grid(row = 1)
-    text_box2 = Text(middle).grid(row = 2)
-    text_box3 = Text(middle).grid(row = 3)
+    Label(middle, text="GRATITUDE & ACCEPTANCE", font=("Arial", 16)).grid(row = 0)
+    for i in range(3):
+        text_G.append(Text(middle, width=100, height=2))
+        text_G[i].grid(row = i + 1)
+
+    
+def update():
+    updateStreaks()
+    createTxt()
+
+def createTxt():
+    print("aa")
 
 def updateStreaks():
     with open("../streaks/streaks.json", "r+") as jsonFile:
