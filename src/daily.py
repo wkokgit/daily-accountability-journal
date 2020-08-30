6132fb9fd6c5b94efefc4f71a46ec54e3a7093f6
wkokgit <wouterkok96@hotmail.com>
2020-08-30 16:57:20 +0200

    added pdf integration

--- src/daily.py
+++ src/daily.py
@@ -1,4 +1,6 @@
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
 from tkinter import *
 from datetime import date
 import json
@@ -9,17 +11,14 @@ entries_S = []
 text_G = []
 planningContent = []
 
-today = date.today()
+todaysDate = date.today()
+todaysDateFormatted = todaysDate.strftime("%B %d, %Y")
 
 root=Tk()
+root.tk_setPalette(background='grey7', foreground="ghost white", activeBackground='ghost white')
+# , foreground='black', activeBackground='black', activeForeground='grey7')
 
-STARTFRAME = Frame(root)
-STARTFRAME.pack()
-planningF = Frame(root)
-planningF.pack()
 
-ENDFRAME = Frame(root)
-ENDFRAME.pack()
 top=Frame(root)
 top.pack()
 
@@ -37,6 +36,8 @@ streaksText.pack(side=TOP)
 streaksList = Frame(streaks_F)
 streaksList.pack()
 
+planningF = Frame(root)
+planningF.pack()
 
 middle=Frame(root, pady=3)
 middle.pack()
@@ -45,19 +46,14 @@ bottom.pack()
 
 
 def main():
-    root.title("Daily Practice - " + today.strftime("%B %d, %Y"))
-    
-    #START OF DAY
-    START = Label(STARTFRAME, width=40, text="START OF DAY", font=("Arial", 22))
-    START.pack()
+    root.title("Daily Practice - " + todaysDateFormatted)
+
     planning()
 
-    #END OF DAY
-    END = Label(ENDFRAME, width=40, text="END OF DAY", font=("Arial", 22))
-    END.pack()
+
     introduction()
     streaks()
-    
+
     gratitude()
 
     B = Button(bottom, text ="Save", command = update)
@@ -70,10 +66,10 @@ def main():
 def planning():
     with open("C:/Users/Wouter Kok/Documents/self-improj/data/planning.json", "r+") as jsonFile:
         data = json.load(jsonFile)
-        planningLabel = Label(planningF, width=40, text="PLANNING", font=("Arial", 16))
+        planningLabel = Label(planningF, width=40, text="LIST OF EVENTS & LEARNINGS", font=("Arial", 16))
         planningLabel.pack()
-        planningText = Text(planningF, width=100, height=4)
-        
+        planningText = Text(planningF, width=100, height=4, bg='ghost white', fg="black")
+
 
         planningText.insert(INSERT, data['planning'])
         planningContent.insert(0, planningText)
@@ -83,11 +79,11 @@ def planning():
 def introduction():
     greeting = Label(introTop, width=40, text="EXTRA INFO", font=("Arial", 16))
     greeting.pack()
-    
-    strin =  """-- SLEEP SCHEDULE : 6:00 OUT - 21:30 IN -- 
+
+    strin =  """SLEEP SCHEDULE : 6:00 OUT - 21:30 IN
                 ...
                 ...
-                ..."""  
+                ..."""
     moreInfo = Label(introText, text=strin, font = ("Arial", 12))
     moreInfo.pack()
 
@@ -100,16 +96,16 @@ def streaks():
 def readStreaks():
     with open("C:/Users/Wouter Kok/Documents/self-improj/data/streaks.json", "r+") as jsonFile:
         data = json.load(jsonFile)
-        
+
         i = 0
         for k,v in data.items():
             labels_S.append(Label(streaksList, text=k, font=("Arial",12)))
             labels_S[i].grid(row = i)
 
-            entries_S.append(Entry(streaksList))
+            entries_S.append(Entry(streaksList, justify='center'))
             entries_S[i].grid(row=i, column=1)
             entries_S[i].insert(0, v)
-            
+
             i = i + 1
 
 def gratitude():
@@ -118,7 +114,7 @@ def gratitude():
         text_G.append(Text(middle, width=100, height=3))
         text_G[i].grid(row = i + 1)
 
-    
+
 def update():
     updateStreaks()
     updatePlanning()
@@ -144,15 +140,26 @@ def updatePlanning():
     with open("../data/planning.json", "r+") as jsonFile:
         data = json.load(jsonFile)
 
-    print(planningContent[0].get(1.0,END))
     data['planning'] = planningContent[0].get(1.0, END)
 
     with open("../data/planning.json", "w") as jsonFile:
         json.dump(data, jsonFile)
 
 def test_template():
-    writer = PdfWriter()
+    pdfName = '../results/' + todaysDateFormatted + '.pdf'
+    my_doc = SimpleDocTemplate(pdfName)
+    sample_style_sheet = getSampleStyleSheet()
+    flowables = []
 
-    writer.write('../results/2.pdf')
+    paragraph_1_text = 'The day of ' + pdfName
+    paragraph_1 = Paragraph(paragraph_1_text, sample_style_sheet['Heading1'])
+    paragraph_2 = Paragraph(
+        "Some normal body text",
+        sample_style_sheet['BodyText']
+    )
+    flowables.append(paragraph_1)
+    flowables.append(paragraph_2)
+    my_doc.build(flowables)
+
 
 main()
