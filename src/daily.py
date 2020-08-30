5819d90db3b7ce56b29f1af4b411bce0ad0b9ee1
wkokgit <wouterkok96@hotmail.com>
2020-08-30 17:48:55 +0200

    update pdf

--- src/daily.py
+++ src/daily.py
@@ -15,33 +15,41 @@ todaysDate = date.today()
 todaysDateFormatted = todaysDate.strftime("%B %d, %Y")
 
 root=Tk()
+default_font = "Verdana"
+root.option_add("*Font", default_font)
+root.geometry("1400x750")
+root.option_add("*Button.Background", "ghost white")
+root.option_add("*Button.Foreground", "grey7")
+root.option_add("*Text.Background", "ghost white")
+root.option_add("*Text.Foreground", "black")
 root.tk_setPalette(background='grey7', foreground="ghost white", activeBackground='ghost white')
 # , foreground='black', activeBackground='black', activeForeground='grey7')
 
+planningF = Frame(root)
+planningF.pack()
+
+middle=Frame(root)
+middle.pack()
 
 top=Frame(root)
 top.pack()
 
-intro=Frame(top, pady=3)
+intro=Frame(top)
 intro.pack(side=LEFT)
 introTop=Frame(intro)
 introTop.pack(side=TOP)
 introText=Frame(intro)
 introText.pack()
 
-streaks_F=Frame(top, pady=3)
+streaks_F=Frame(top)
 streaks_F.pack(side=RIGHT)
 streaksText=Frame(streaks_F)
 streaksText.pack(side=TOP)
 streaksList = Frame(streaks_F)
 streaksList.pack()
 
-planningF = Frame(root)
-planningF.pack()
 
-middle=Frame(root, pady=3)
-middle.pack()
-bottom=Frame(root, pady=3)
+bottom=Frame(root)
 bottom.pack()
 
 
@@ -66,10 +74,11 @@ def main():
 def planning():
     with open("C:/Users/Wouter Kok/Documents/self-improj/data/planning.json", "r+") as jsonFile:
         data = json.load(jsonFile)
-        planningLabel = Label(planningF, width=40, text="LIST OF EVENTS & LEARNINGS", font=("Arial", 16))
+        planningLabel = Label(planningF, width=40, text="LIST OF EVENTS & LEARNINGS", font=(default_font, 16))
         planningLabel.pack()
-        planningText = Text(planningF, width=100, height=4, bg='ghost white', fg="black")
+        planningText = Text(planningF, width=100, height=8)
 
+        planningText.config(padx = 15, pady = 15)
 
         planningText.insert(INSERT, data['planning'])
         planningContent.insert(0, planningText)
@@ -77,19 +86,19 @@ def planning():
 
 
 def introduction():
-    greeting = Label(introTop, width=40, text="EXTRA INFO", font=("Arial", 16))
+    greeting = Label(introTop, width=40, text="EXTRA INFO", font=(default_font, 16))
     greeting.pack()
 
     strin =  """SLEEP SCHEDULE : 6:00 OUT - 21:30 IN
                 ...
                 ...
                 ..."""
-    moreInfo = Label(introText, text=strin, font = ("Arial", 12))
+    moreInfo = Label(introText, text=strin, font = (default_font, 12))
     moreInfo.pack()
 
 
 def streaks():
-    greeting = Label(streaksText, width=40, text="STREAKS", font=("Arial", 16))
+    greeting = Label(streaksText, width=40, text="STREAKS", font=(default_font, 16))
     greeting.pack(side=RIGHT)
     readStreaks()
 
@@ -99,9 +108,8 @@ def readStreaks():
 
         i = 0
         for k,v in data.items():
-            labels_S.append(Label(streaksList, text=k, font=("Arial",12)))
+            labels_S.append(Label(streaksList, text=k, font=(default_font,12)))
             labels_S[i].grid(row = i)
-
             entries_S.append(Entry(streaksList, justify='center'))
             entries_S[i].grid(row=i, column=1)
             entries_S[i].insert(0, v)
@@ -109,9 +117,11 @@ def readStreaks():
             i = i + 1
 
 def gratitude():
-    Label(middle, text="GRATITUDE & ACCEPTANCE", font=("Arial", 16)).grid(row = 0)
+    Label(middle, text="GRATITUDE & ACCEPTANCE", font=(default_font, 16)).grid(row = 0)
     for i in range(3):
-        text_G.append(Text(middle, width=100, height=3))
+        newText = Text(middle, width=100, height=1.5)
+        newText.config(padx = 15, pady = 15)
+        text_G.append(newText)
         text_G[i].grid(row = i + 1)
 
 
@@ -151,14 +161,21 @@ def test_template():
     sample_style_sheet = getSampleStyleSheet()
     flowables = []
 
-    paragraph_1_text = 'The day of ' + pdfName
+    paragraph_1_text = 'The day of ' + todaysDateFormatted
     paragraph_1 = Paragraph(paragraph_1_text, sample_style_sheet['Heading1'])
-    paragraph_2 = Paragraph(
-        "Some normal body text",
-        sample_style_sheet['BodyText']
-    )
-    flowables.append(paragraph_1)
-    flowables.append(paragraph_2)
+
+    paragraph_2 = Paragraph("LIST OF EVENTS & LEARNINGS", sample_style_sheet['Heading2'])
+    paragraph_3 = Paragraph(planningContent[0].get(1.0, END).replace('\n','<br />\n'), sample_style_sheet['BodyText'])
+
+    paragraph_4 = Paragraph("GRATITUDE & ACCEPTANCE", sample_style_sheet['Heading2'])
+
+    temp = ""
+    for i in range(3):
+        temp = temp + " - " + text_G[i].get(1.0, END) + "\n"
+
+    paragraph_5 = Paragraph(temp.replace('\n','<br />\n'), sample_style_sheet['BodyText'])
+
+    flowables.extend([paragraph_1, paragraph_2, paragraph_3, paragraph_4, paragraph_5])
     my_doc.build(flowables)
 
 
